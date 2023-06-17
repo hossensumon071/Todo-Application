27f449b5d699b7b8b2354094d6e2e2b2590ea541
hossensumon071 <55638643+hossensumon071@users.noreply.github.com>
2023-06-17 13:51:55 +0600

    project added

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,124 @@
-import { useEffect, useState } from "react"
-import AddTask from "./components/AddTask"
-import Footer from "./components/Footer"
-import Header from "./components/Header"
-import TaskList from "./components/TaskList"
+import { createContext, useEffect, useState } from "react";
+import AddTask from "./components/AddTask";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import TaskList from "./components/TaskList";
+
+export const DeleteHandlerContext = createContext();
+export const EditHandlerContext = createContext();
 
 const App = () => {
   const [tasks, setTasks] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+
+  const [editedText, setEditedText] = useState("");
+  const [toggleEditMode, setToggleEditMode] = useState(true);
 
   useEffect(() => {
     // getting data from the server
-    fetchingData()
-  }, [])
+    fetchingData();
+  }, []);
 
-  // getting data from the server 
+  // getting data from the server
   const fetchingData = async () => {
     try {
-      const res = await fetch("https://whispering-dour-beechnut.glitch.me/tasks");
-      if(!res.ok) throw new Error("Something went wrong");
+      const res = await fetch(
+        "https://whispering-dour-beechnut.glitch.me/tasks"
+      );
+      if (!res.ok) throw new Error("Something went wrong");
       const data = await res.json();
-      setTasks(data)
-    } catch(error) {
-      console.log(error.message)
+      setTasks(data);
+      setLoading(false);
+    } catch (error) {
+      setError(error.message);
     }
-  }
+  };
 
+  // delete event
+  const handleDelete = (id) => {
+    // delete data
+    deleteData(id);
+    // set updated Tasks
+    setTasks(tasks.filter((task) => id !== task.id));
+  };
 
-  
+  const deleteData = async (id) => {
+    await fetch(`https://whispering-dour-beechnut.glitch.me/tasks/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-type": "application/json",
+      },
+    });
+  };
+
+  // editing event
+  const handleEdit = (id) => {
+    const [editableTarget] = tasks.filter((task) => id === task.id);
+    editableTarget.isEditable = true;
+    setEditedText(editableTarget.text);
+
+    setTasks([...tasks]);
+    setToggleEditMode(false);
+
+    // re-arrange
+    tasks
+      .filter((task) => task.id !== id)
+      .map((targetedEl) => (targetedEl.isEditable = false));
+  };
+
+  // editing task handler
+  const handleEditSubmitter = (e, id) => {
+    e.preventDefault();
+
+    setToggleEditMode(!toggleEditMode);
+
+    // parsist data
+    const editParsistance = {
+      text: editedText,
+      id: id,
+    };
+
+    // put request
+    puttingRequest(id, editParsistance);
+
+    // real time update
+    const [editableTarget] = tasks.filter((task) => id === task.id);
+    editableTarget.isEditable = false;
+    editableTarget.text = editParsistance.text;
+
+    setTasks([...tasks]);
+  };
+
+  const puttingRequest = async (id, newData) => {
+    fetch(`https://whispering-dour-beechnut.glitch.me/tasks/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(newData),
+    });
+  };
+
   return (
     <div className="wrapper bg-gradient-to-t from-gray-900 to-teal-900 min-h-screen text-xl text-gray-100 flex flex-col py-10">
-      <Header/>
-      <AddTask tasks={tasks} setTasks={setTasks}/>
-      <TaskList tasks={tasks}/>
-      <Footer/>
+      <DeleteHandlerContext.Provider value={handleDelete}>
+        <EditHandlerContext.Provider value={handleEdit}>
+          <Header />
+          <AddTask tasks={tasks} setTasks={setTasks} />
+          <TaskList
+            tasks={tasks}
+            error={error}
+            loading={loading}
+            handleEditSubmitter={handleEditSubmitter}
+            editedText={editedText}
+            setEditedText={setEditedText}
+          />
+          <Footer />
+        </EditHandlerContext.Provider>
+      </DeleteHandlerContext.Provider>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,10 +1,29 @@
 import TaskItem from "./TaskItem";
 
-const TaskList = ({ tasks }) => {
+const TaskList = ({
+  tasks,
+  error,
+  loading,
+  handleEditSubmitter,
+  editedText,
+  setEditedText,
+}) => {
   return (
     <div className="container mx-auto flex-flex-col gap-3 bg-gray-900 p-10">
+      {loading ? (
+        <p className="text-center">{error ? error : "Loading..."}</p>
+      ) : (
+        tasks.length === 0 && <p className="text-center">Not Task to Show</p>
+      )}
+
       {tasks.map((task) => (
-        <TaskItem task={task} key={task.id} />
+        <TaskItem
+          task={task}
+          key={task.id}
+          handleEditSubmitter={handleEditSubmitter}
+          editedText={editedText}
+          setEditedText={setEditedText}
+        />
       ))}
     </div>
   );
